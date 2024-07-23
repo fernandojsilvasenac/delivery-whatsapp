4dc8129db08ee15931166b8245d9371ece487294
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2024-07-22 22:26:33 -0300

    updated header in TouchableOpacity, 'category-button added Pressable, lib clsx', added FlatList and SectionList in index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,57 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { View, Text, FlatList, SectionList } from "react-native";
 import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
+import { CATEGORIES, MENU } from "@/utils/data/products";
 
 export default function Home(){
+    const [category, setCategory] = useState(CATEGORIES[0])
+    function handleCategorySelect(selectedCategory: string){
+        setCategory(selectedCategory)
+        const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory)
+    }
+
     return(
         <View>
-            <Header title="Faça seu Pedido" />
-            <Header title="Sua sacolinha"/>
+            <Header title="Faça seu Pedido" cartQuantityItems={0} />
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({item}) =>
+                    <CategoryButton
+                        title={item}
+                        isSelected={item === category}
+                        onPress={() =>handleCategorySelect(item)}
+                    />    
+                }
+                horizontal
+                className="max-h-10 mt-1"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{gap:12, paddingHorizontal:20}}
+            />
+            <SectionList
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                // não fazer efeito de esticar
+                stickySectionHeadersEnabled={false}
+                renderItem={({item}) =>
+                    <Text className="text-white">{item.title}</Text>
+                }
+                renderSectionHeader={({section: {title}}) => (
+                    <Text className="text-yellow-200">{title}</Text>
+                )}
+            />
+
         </View>
         )
 }
+
+
+{/* 
+<View className="flex-row gap-8">
+<CategoryButton title="Lanche do dia"  />
+<CategoryButton title="Promoções" isSelected />
+<CategoryButton title="Sobremesa" />
+<CategoryButton title="Bebidas" />
+</View> 
+*/}
